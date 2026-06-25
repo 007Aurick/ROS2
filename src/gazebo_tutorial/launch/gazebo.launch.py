c554bbdbbccae384f9292b5032f282391f1cfdd5
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-06-25 18:39:27 -0400

    Added Optimus Bot

--- src/gazebo_tutorial/launch/gazebo.launch.py
+++ src/gazebo_tutorial/launch/gazebo.launch.py
@@ -1,55 +1,61 @@
 import os
-
-import xacro
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 
 
 def generate_launch_description():
-    pkg_path = get_package_share_directory('gazebo_tutorial')
-    xacro_file = os.path.join(pkg_path, 'urdf', 'teslabot.urdf.xacro')
-    robot_desc = xacro.process_file(xacro_file).toxml()
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('gazebo_ros'),
-                'launch',
-                'gazebo.launch.py',
-            )
-        )
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([FindPackageShare('gazebo_ros'),
+                'launch','gazebo.launch.py'])])
     )
 
-    robot_state_publisher = Node(
+    robot_description = Command([
+        FindExecutable(name='xacro'), ' ',
+        PathJoinSubstitution([
+            FindPackageShare('gazebo_tutorial'), 'urdf', 'teslabot.urdf.xacro'
+        ])
+    ])
+
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_desc,
-        }],
+        parameters=[{'robot_description': robot_description}],
+        output='screen'
     )
 
-    spawn_entity = Node(
-        package='gazebo_ros',
+    robot_spawn_node = Node(
+        package='gazebo_ros', 
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'teslabot', '-z', '0.2'],
-        output='screen',
+        arguments=['-topic', 'robot_description',
+                   '-entity', 'robot'],
+        output='screen'
+    )
+    
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
     )
 
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_trajectory_controller'],
+        output='screen'
+    )
+   
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true',
-        ),
-        gazebo,
-        robot_state_publisher,
-        spawn_entity,
+        gazebo_launch,
+        robot_state_publisher_node,
+        robot_spawn_node,
+        load_joint_state_broadcaster,
+        load_joint_trajectory_controller
     ])
